1c691f22c989d482d622e69f281e11dcf2ecce2f
Student <example@example.com>
2019-10-28 16:31:17 -0700

    replace if/else statement with filter()

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,11 @@ $(document).ready(function() {
   $('#user-input').submit(function(event) {
     event.preventDefault();
     $('#user-input').hide();
-    var threeOrMoreChar = [];
-    var noHome = [];
     var userSentence = $('#user-sentence').val();
     var splitSentence = userSentence.split(' ');
     splitSentence.forEach(function(word) {
-      if (word.length >= 3) {
-        threeOrMoreChar.push(word);
-      } else {
-        noHome.push(word);
-      }
-      var reverseThreeOrMoreChar = threeOrMoreChar.reverse().join(' ');
+    var result = splitSentence.filter(word => word.length > 3);
+      var reverseThreeOrMoreChar = result.reverse().join(' ');
       $('#fav-list').text(reverseThreeOrMoreChar);
     });
   });
